f0d19d7108ab0169ad68d4375a393ebaba8aaee0
Thales Flores Testoni <thales.testoni@havan.com.br>
2022-09-18 11:04:28 -0300

    Logging cycle time

--- app/src/main/java/ifsc/testoni/hello/MainActivity.java
+++ app/src/main/java/ifsc/testoni/hello/MainActivity.java
@@ -3,6 +3,7 @@ package ifsc.testoni.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("cycleTime", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("cycleTime", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("cycleTime", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("cycleTime", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("cycleTime", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("cycleTime", "onDestroy");
     }
 }
